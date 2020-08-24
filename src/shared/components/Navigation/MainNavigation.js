cd4889d1f12daea751626fe71cd16712255b391b
RikeshThapa <rthapa@u.rochester.edu>
2020-08-23 23:24:07 -0500

    Adding animations and CSSTransitions so that there is a nice effect on menu slide in and out for mobile view

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -13,28 +13,26 @@ const MainNavigation = props => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
     
     //toggle drawerstate
-    const openDrawer = () => {
+    const openDrawerHandler = () => {
         setDrawerIsOpen(true);
     };
 
-    const closeDrawer = () => {
+    const closeDrawerHandler = () => {
         setDrawerIsOpen(false);
     }
 
     return (
         <React.Fragment>
             {drawerIsOpen && (
-                <Backdrop onClick={closeDrawer}/>
+                <Backdrop onClick={closeDrawerHandler}/>
             )}
-            {drawerIsOpen && (
-            <SideDrawer>
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
-            )}
             <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawer}>
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                     <span />
                     <span />
                     <span />
